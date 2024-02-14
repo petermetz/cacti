507fb15d6feac85aba7e2904bb43433a01262e79
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-02-13 16:18:38 -0800

    build(connector-corda): upgrade to Spring Boot v3
    WORK IN PROGRESS - this needs to be broken down into a
    few separate commits to make it easier to review.
    
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-ledger-connector-corda/src/main-server/kotlin/gen/kotlin-spring/src/main/kotlin/org/hyperledger/cactus/plugin/ledger/connector/corda/server/impl/ApiPluginLedgerConnectorCordaServiceImpl.kt
+++ packages/cactus-plugin-ledger-connector-corda/src/main-server/kotlin/gen/kotlin-spring/src/main/kotlin/org/hyperledger/cactus/plugin/ledger/connector/corda/server/impl/ApiPluginLedgerConnectorCordaServiceImpl.kt
@@ -1,7 +1,12 @@
 package org.hyperledger.cactus.plugin.ledger.connector.corda.server.impl
 
+// import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import net.corda.core.contracts.ContractState
 import net.corda.core.flows.FlowLogic
@@ -21,6 +26,7 @@ import org.hyperledger.cactus.plugin.ledger.connector.corda.server.model.*
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
+import org.springframework.stereotype.Service
 import org.springframework.web.util.HtmlUtils.htmlEscape
 import java.io.IOException
 import java.io.InputStream
@@ -343,9 +349,10 @@ class ApiPluginLedgerConnectorCordaServiceImpl(
 
     override fun networkMapV1(body: Any?): List<NodeInfo> {
         val reader = mapper.readerFor(object : TypeReference<List<NodeInfo?>?>() {})
-
         val networkMapSnapshot = rpc.proxy.networkMapSnapshot()
-        val networkMapJson = writer.writeValueAsString(networkMapSnapshot)
+        val serializer = CordaNetworkMapSnapshotJsonSerializer(networkMapSnapshot)
+        val x = serializer.asListOfMaps()
+        val networkMapJson = writer.writeValueAsString(x)
         logger.trace("networkMapSnapshot=\n{}", networkMapJson)
 
         val nodeInfoList = reader.readValue<List<NodeInfo>>(networkMapJson)
