749c002a1bee57c0d5c5ca880955782ba7143925
Peter Somogyvari <peter.somogyvari@accenture.com>
2024-10-01 00:23:15 -0700

    squash! - not yet sure
    Signed-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>

--- packages/cactus-plugin-satp-hermes/src/main/typescript/gol/gateway-orchestrator.ts
+++ packages/cactus-plugin-satp-hermes/src/main/typescript/gol/gateway-orchestrator.ts
@@ -117,7 +117,7 @@ export class GatewayOrchestrator {
           routes: this.handlers
             .get(stage)!
             .setupRouter.bind(this.handlers.get(stage)!),
-        }),
+        }) as any, // FIXME(petermetz)
       );
     }
   }
